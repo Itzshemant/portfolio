18f8b3dfeacdcfa845d2d4ec75b3903e11ee1f95
Hemant Mahajan <mahajanhemant078@gmail.com>
2025-06-15 18:36:31 +0530

    Initial commit

---
+++ index2.js
@@ -0,0 +1,17 @@
+// const text = document.querySelector(".sec-text");
+// const textload =()=>{
+//     setTimeout (()=>{
+//         text.textContent= " Developer"
+//     }, 0);
+//     setTimeout (()=>{
+//         text.textContent= " Designer"
+//     }, 3000);
+//     setTimeout (()=>{
+//         text.textContent= " VideoEditor"
+//     }, 6000);
+//     setTimeout (()=>{
+//         text.textContent= " CanvaEditor"
+//     }, 9000);
+// }
+
+// textload();
